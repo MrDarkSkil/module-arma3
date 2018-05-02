d56c8ba40e5a1e58944d3dbcffa59398cfae116c
Hubert Léo <leohub@live.fr>
2018-05-02 17:12:24 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
-
-export default class {
-
-    readonly name = 'TEST';
-
-    readonly version = '0.0.1';
-
-    constructor() {
-        console.log('salut :)');
-    }
-
-    init() {
-        console.log('fgrjhfgr');
-    }
-}
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var default_1 = /** @class */ (function () {
+        function default_1() {
+            this.name = 'TEST';
+            this.version = '0.0.1';
+            console.log('salut :)');
+        }
+        default_1.prototype.init = function () {
+            console.log('fgrjhfgr');
+        };
+        return default_1;
+    }());
+    exports.default = default_1;
+});
